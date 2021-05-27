3d13cac21e4480634f538d7ead77ef88c270bd1e
Pedro Henrique Cavalcante de Sousa <pedro.henrique.5566@gmail.com>
2021-05-27 09:36:44 -0300

    Created Utils for the tests

--- DesafioBTGTests/Utils/Utils.swift
+++ DesafioBTGTests/Utils/Utils.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+class Utils {
+    class func loadJSONData(for anyClass: AnyClass, fileName: String) -> Data {
+        guard let url = Bundle(for: anyClass).url(forResource: fileName, withExtension: "json"),
+              let data = try? Data(contentsOf: url)
+        else {
+            assertionFailure("Failed to load mock data")
+            return Data()
+        }
+        
+        return data
+    }
+}
